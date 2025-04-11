733b9adb91ab6c7a419b60afcd8b91a22a42d589
itaisoudry <itaisoudry@gmail.com>
2025-04-11 11:52:43 +0700

    added localhost support

--- src/temporal.service.ts
+++ src/temporal.service.ts
@@ -6,17 +6,21 @@ export default class TemporalService {
   headers: Record<string, string>;
   constructor() {
     this.apiKey = process.env.TEMPORAL_API_KEY ?? "";
-    this.endpoint = process.env.TEMPORAL_ENDPOINT ?? "";
+    const temporalEndpoint = process.env.TEMPORAL_ENDPOINT ?? "";
+    if (!temporalEndpoint) {
+      throw new Error(
+        "Temporal Endpoint is required - set TEMPORAL_ENDPOINT envvar"
+      );
+    }
+    if (temporalEndpoint.includes("localhost")) {
+      this.endpoint = temporalEndpoint;
+    } else {
+      this.endpoint = `${temporalEndpoint}.web.tmprl.cloud`;
+    }
 
     if (!this.apiKey) {
       throw new Error(
         "Temporal API Key is required - set TEMPORAL_API_KEY envvar"
-      );
-    }
-
-    if (!this.endpoint) {
-      throw new Error(
-        "Temporal Endpoint is required - set TEMPORAL_ENDPOINT envvar"
       );
     }
 
@@ -28,7 +32,7 @@ export default class TemporalService {
   async searchWorkflows(query: string, namespace: string) {
     const url = `https://${
       this.endpoint
-    }.web.tmprl.cloud/api/v1/namespaces/${namespace}/workflows?query=${encodeURIComponent(
+    }/api/v1/namespaces/${namespace}/workflows?query=${encodeURIComponent(
       query
     )}`;
     const response = await fetch(url, { headers: this.headers });
@@ -41,7 +45,7 @@ export default class TemporalService {
   }
 
   async getWorkflowData(namespace: string, workflowId: string, runId: string) {
-    const url = `https://${this.endpoint}.web.tmprl.cloud/api/v1/namespaces/${namespace}/workflows/${workflowId}?execution.runId=${runId}`;
+    const url = `https://${this.endpoint}/api/v1/namespaces/${namespace}/workflows/${workflowId}?execution.runId=${runId}`;
     const response = await fetch(url, { headers: this.headers });
     if (!response.ok) {
       throw new InternalServerError(
@@ -56,7 +60,7 @@ export default class TemporalService {
     workflowId: string,
     runId: string
   ) {
-    const url = `https://${this.endpoint}.web.tmprl.cloud/api/v1/namespaces/${namespace}/workflows/${workflowId}/history?execution.runId=${runId}&next_page_token=`;
+    const url = `https://${this.endpoint}/api/v1/namespaces/${namespace}/workflows/${workflowId}/history?execution.runId=${runId}&next_page_token=`;
     const response = await fetch(url, { headers: this.headers });
     if (!response.ok) {
       throw new InternalServerError(
